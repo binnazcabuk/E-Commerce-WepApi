ab2ed68d665a84a461075c006eaee87d0ad78692
binnazcabuk <34273337+binnazcabuk@users.noreply.github.com>
2022-02-22 09:49:43 +0300

    Update IBasketDetailService.cs

--- Business/Abstract/IBasketDetailService.cs
+++ Business/Abstract/IBasketDetailService.cs
@@ -9,18 +9,11 @@ namespace Business.Abstract
 {
     public interface IBasketDetailService
     {
-
-
         IResult Add(BasketDetail basketDetail);
         IResult Delete(BasketDetail basketDetail);
         IResult Update(BasketDetail basketDetail);
         IDataResult<List<BasketDetailDto>> GetAllBasket(int userId);
         IDataResult<List<BasketDetail>> GetAll(int userId);
         IDataResult<List<BasketDetail>> GetCartById(int basketId);
-
-
-
-
-
     }
 }
